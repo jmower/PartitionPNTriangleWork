d7581b40541d03ab043cc9fcaade053ebbb80156
jmower <jmower@albany.edu>
2016-07-12 09:21:20 -0400

    Committed original code

--- PartitionPNTriangleWork/main.cpp
+++ PartitionPNTriangleWork/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include <glm/glm.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    glm::uvec2 agDim(8,16); // Patch dimensions (cols, rows) for some arbgrid
+    uint totalPatches = agDim.x * agDim.y;
+    const uint PHYSICALPROCESSORS = 2034; // Total number of processors for NVIDIA GTX 780 GPU
+    uint tessVertsPerPatch = 36; // This will be a variable in real application
+    uint totalVerticesInAg = totalPatches * tessVertsPerPatch;
+    uint maxConcurrentPatches = PHYSICALPROCESSORS / tessVertsPerPatch;
+    uint fullConcurrentPasses = totalPatches / maxConcurrentPatches;
+    uint leftoverPatches = totalPatches % maxConcurrentPatches;
+   
+    std::cout << "For a patch grid with " << agDim.x << " cols and " << agDim.y << " rows, " << std::endl;
+    std::cout << "containing " << tessVertsPerPatch << " vertices per patch " << std::endl;
+    std::cout << "for a total of " << totalVerticesInAg << " vertices overall, " << std::endl;
+    std::cout << "you can process " << maxConcurrentPatches << " patches concurrently, " << std::endl;
+    //std::cout << "with " << leftoverPatches << " patches leftover " << std::endl;
+    std::cout << "covering " << totalPatches << " total patches on " << fullConcurrentPasses <<
+        " full passes with " << std::endl;
+    std::cout << "a final partial pass containing " << leftoverPatches << " leftovers." << std::endl;
     return 0;
 }
